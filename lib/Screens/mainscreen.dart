e91bafba6cfa7d78e84453a9e2e593ad77d903be
kushhmishra45-droid <kushhmishra45@gmail.com>
2026-07-13 00:43:23 +0530

    profile screen working properly

--- lib/Screens/mainscreen.dart
+++ lib/Screens/mainscreen.dart
@@ -18,17 +18,22 @@ class Mainscreen extends StatelessWidget {
           Uihelper.CustomText(text: "Please provide your name and an optional" , height: 16 ,color: Color(0XFF5E5E5E),fontWeight: FontWeight.bold),
           Uihelper.CustomText(text: "profile picture." , height: 16,color: Color(0XFF5E5E5E),fontWeight: FontWeight.bold),
           SizedBox(height: 50,),
-          CircleAvatar(
-            radius: 104,
-            backgroundColor: Color(0XFFD9D9D9),
-            child: Icon(Icons.person, size: 50, color: Color(0XFF5E5E5E),),
+          GestureDetector(
+            onTap: () {
+              _openImagePicker(context);
+            },
+            child: CircleAvatar(
+              radius: 104,
+              backgroundColor: Color(0XFFD9D9D9),
+              child: Icon(Icons.person, size: 50, color: Color(0XFF5E5E5E),),
+            ),
           ),
           SizedBox(height: 50,),
   Row(
      mainAxisAlignment: MainAxisAlignment.center,
     children: [
       SizedBox(
-        width: 300,
+        width: 200,
         child: TextField(
          keyboardType: TextInputType.name,
           decoration: InputDecoration(
@@ -54,6 +59,38 @@ class Mainscreen extends StatelessWidget {
         Navigator.push(context, MaterialPageRoute(builder: (context) => HomeScreen()));
        } ,buttonname: "Next", ),
        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+  _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          width: 200,
+          color: Color(0XFFD9D9D9),
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.camera_alt),
+                title: Text('Take a photo'),
+                onTap: () {
+                  // Handle camera option
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_library),
+                title: Text('Choose from gallery'),
+                onTap: () {
+                  // Handle gallery option
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
