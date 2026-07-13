3c2c02e704c4788b10d52a71237a288d675543a9
kushhmishra45-droid <kushhmishra45@gmail.com>
2026-07-13 23:58:38 +0530

    Completed Home screen Functionalities

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -1,3 +1,8 @@
+import 'package:begining_of_new_chapter/Screens/callscreen.dart';
+import 'package:begining_of_new_chapter/Screens/camerascreen.dart';
+import 'package:begining_of_new_chapter/Screens/chatscreen.dart';
+import 'package:begining_of_new_chapter/Screens/statusscreen.dart';
+import 'package:begining_of_new_chapter/widgets/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,14 +10,40 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Home Screen"),
-          ],
-        ),
+    return DefaultTabController(
+      length: 4, 
+      child: Scaffold(        
+              appBar: AppBar(
+                elevation: 0,
+                bottom:  TabBar(
+                  tabs: [
+                    Tab(icon: Icon(Icons.camera_alt)),
+                    Tab(text: "CHATS"),
+                    Tab(text: "STATUS"),
+                    Tab(text: "CALLS"),
+                  ],indicatorColor: Colors.white
+                ),
+                toolbarHeight: 125,
+                title: Uihelper.CustomText(text: "Whatsapp", height: 20, color: Colors.white),
+                actions: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: IconButton(
+                      icon: Image.asset("assets/Search.png",),
+                      onPressed: () {
+                        // Handle search button press
+                      },
+                    ),
+                  ),
+                ], 
+              ), 
+              body: TabBarView(children:  [
+                CameraScreen(),
+                ChatScreen(),
+                StatusScreen(),
+                CallsScreen(),
+              ]),
+        
       ),
     );
   }
